5347eb357c35f75e6ca7d168dd4b1d483c4e5ed7
Carlos Marinho <carluizfla@hotmail.com>
2022-07-23 00:07:29 -0300

    add banner to home with css

--- pages/_document.js
+++ pages/_document.js
@@ -7,16 +7,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet" />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap'
+            rel='stylesheet'
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
